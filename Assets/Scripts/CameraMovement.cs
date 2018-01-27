f1917773e5b68e57f733984b6fabb69b85cf8122
AaronWeingarten <aarweing@gmail.com>
2018-01-26 20:50:09 -0800

    updated file

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform Player;
+    public float mouseSensitivity = 100.0f;
+    public float clampAngleY;
+    private float clampUp;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        clampUp = clampAngleY - 90;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
+
+        rotX = Mathf.Clamp(rotX, -clampAngleY + clampUp, clampAngleY);
+
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+        Quaternion playerRotation = Quaternion.Euler(Player.rotation.x, rotY, 0.0f);
+        Player.rotation = playerRotation;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+
+    [SerializeField] private int mspeed;
+    [SerializeField] private float jumpHeight = 5f;
+    public bool isGrounded;
+    public bool isWall;
+    public bool crouch;
+    public Transform groundCheck;
+    //public Transform wallCheck;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        //isWall = Physics.Linecast(transform.position, wallCheck.position, 1 << LayerMask.NameToLayer("Wall"));
+        isGrounded = Physics.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
+        {
+            transform.position += transform.forward * Time.deltaTime * mspeed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * Time.deltaTime * mspeed;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * Time.deltaTime * mspeed;
+            //transform.Rotate(Vector3.up * Time.deltaTime * rspeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position -= transform.right * Time.deltaTime * mspeed;
+            //transform.Rotate(-Vector3.up * Time.deltaTime * rspeed);
+        }
+
+        if (isGrounded)
+        {
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (!crouch)
+                {
+                    //print("jump");
+                    GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight);
+                    isGrounded = false;
+                }
+            }
+        }
+    }
+}

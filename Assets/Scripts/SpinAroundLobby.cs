3acfcb98cae23dccd529897f0115bed588201d02
midgear <terra@terrasnover.com>
2018-01-28 13:42:34 -0800

    Menu and Lava

---
+++ Assets/Scripts/SpinAroundLobby.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAroundLobby : MonoBehaviour
+{
+	public float Speed;
+	void Update()
+	{
+
+		// ...also rotate around the World's Y axis
+		transform.Rotate(0, Speed * Time.deltaTime, 0, Space.World);
+	}
+}
